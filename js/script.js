f8f8255ed87b59f59d310b4eb5f40d7b1eda52a7
isaacfonseca150@gmail.com <isaacfonseca150@gmail.com>
2024-11-14 12:16:53 -0300

    modularizations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+
 
 // variáveis - variables
 const form = document.querySelector('form')
@@ -12,11 +14,24 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
+    const showAlertError = (notANumber(weight)) || (notANumber(height))
+    
+    if(showAlertError) {
+        AlertError.open()
+        return;                                        
+    }
+
+    AlertError.close()
+
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
-    Modal.message.innerText = message
+    Modal.message.innertext = message
     Modal.open()
+}
+
+function notANumber(value) {
+    return isNaN(value) || value == ""
 }
 
 function IMC(weight, height) {
